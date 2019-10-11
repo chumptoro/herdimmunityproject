15ba55c0a8756839f7d0a2f881d415a700f94ffa
mark <mark.pham.developer@gmail.com>
2019-10-10 23:03:18 -0700

    one test down

--- test.py
+++ test.py
@@ -10,9 +10,12 @@ from simulation import Simulation
 
 def test_create_population():
 	skynet = virus('Skynet', 0.8, 0.3)
-    sim = Simulation(10, 0.5, skynet, 1)
+    sim = Simulation(100, 0.5, skynet, 10)
     sim._create_population()
     assert sim.pop_size == 10
     assert sim.vacc_percentage == 0.5
+    assert sim.initial_infected == 10
+    for 
+
 
 
